e6b16018e91b045493b493755edb626ccfe576d5
Dano_MAC <leonid_gomez@cev.com>
2024-09-24 11:41:23 +0100

    New lines in Swift

--- Prueba_comandos/main.swift
+++ Prueba_comandos/main.swift
@@ -1,11 +1,19 @@
-//
-//  main.swift
-//  Prueba_comandos
-//
-//  Created by user234564 on 9/24/24.
-//
-
-import Foundation
-
-print("Hello, World!")
-
+print("¿Cómo te llamas?")
+var name = readLine()! // El ! fuerza a ser String
+// readLine() es como input() de Python
+var incorrecto = false
+repeat {
+    if name.count > 10 {
+        print("Error: nombre demasiado largo")
+        incorrecto = true
+        name = readLine()!
+    } else if name.first!.isNumber {
+        print("Error: No puede empezar por un número")
+        incorrecto = true
+        name = readLine()!
+    } else {
+        incorrecto = false
+    
+    }
+} while incorrecto
+print("Hola", name)
