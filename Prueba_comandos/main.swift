7bbf91c0a63e0c66a3837560179169ce57609fc9
Dano_MAC <leonid_gomez@cev.com>
2024-09-25 09:09:48 +0100

    New name tests

--- Prueba_comandos/main.swift
+++ Prueba_comandos/main.swift
@@ -1,19 +1,39 @@
+func CaracteresEspeciales(name: String) -> Bool {
+    /*for char in name {
+        if char.isSymbol {
+            return true
+        }
+    }*/
+    let especiales = "¡¿!?@#¢$/()+-*.,:;{}[]"
+    for char in name {
+        for especial in especiales {
+            if char == especial {
+                return true
+            }
+        }
+    }
+    return false
+}
 print("¿Cómo te llamas?")
 var name = readLine()! // El ! fuerza a ser String
 // readLine() es como input() de Python
 var incorrecto = false
 repeat {
-    if name.count > 10 {
+    if name.count > 10 { // Límite dse caracteres
         print("Error: nombre demasiado largo")
         incorrecto = true
         name = readLine()!
-    } else if name.first!.isNumber {
+    } else if name.first!.isNumber { // No empieza por número
         print("Error: No puede empezar por un número")
+        incorrecto = true
+        name = readLine()!
+    } else if CaracteresEspeciales(name: name) { // Tiene caracteres especiales
+        print("Error: tiene caracteres especiales")
         incorrecto = true
         name = readLine()!
     } else {
         incorrecto = false
-    
     }
 } while incorrecto
+
 print("Hola", name)
